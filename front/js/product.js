485fd3a6878ba2b8f317153a5a2a1945bf3091ea
StevenL <lintanf.steven@gmail.com>
2022-05-19 13:24:02 +0200

    insert product details on product page

--- front/js/product.js
+++ front/js/product.js
@@ -7,8 +7,39 @@ let article = '';
 
 // fetch to retrieve api products
 fetch(`http://localhost:3000/api/products/${id}`)
-    .then((response) => response.json())
-    .then((data) => productsIdList(data))
+    .then((response) => {
+        response.json();
+    })
+    .then((data) => {
+        return productsIdList(data);
+    })
     .catch((error) => console.error(error));
 
 // create products article
+function productsIdList(article) {
+    // create img
+    let elementImg = document.createElement('img');
+    document.querySelector('.item__img').appendChild(elementImg);
+    elementImg.src = article.imageUrl;
+    elementImg.alt = article.altTxt;
+
+    // id title
+    let elementTitle = document.getElementById('title');
+    elementTitle.textContent = article.name;
+
+    // id price
+    let elementPrice = document.getElementById('price');
+    elementPrice.textContent = article.price;
+
+    // id description
+    let elementDescription = document.getElementById('description');
+    elementDescription.textContent = article.description;
+
+    // colors
+    for (let colors of article.colors) {
+        let elementsColor = document.createElement('option');
+        document.querySelector('#colors').appendChild(elementsColor);
+        elementsColor.value = colors;
+        elementsColor.textContent = colors;
+    }
+}
